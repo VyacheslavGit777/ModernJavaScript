b2e43d305592f20bf4dfbb68d8d27fcaec51ba39
Vyacheslav <vsvmrk@gmail.com>
2024-03-05 15:36:16 +0300

    Module 10 - 206 Done

--- Module 10 -  Dom & Events (Advanced level)/1 - Simply Bank Web/js/script.js
+++ Module 10 -  Dom & Events (Advanced level)/1 - Simply Bank Web/js/script.js
@@ -9,6 +9,8 @@ const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
 const btnsOpenModalWindow = document.querySelectorAll(
   '.btn--show-modal-window'
 );
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModalWindow = function (e) {
   e.preventDefault();
@@ -36,6 +38,61 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+// Прокручивание страницы
+
+btnScrollTo.addEventListener('click', function (e) {
+  const section1Coords = section1.getBoundingClientRect();
+  console.log(section1Coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(
+    `Текущее прокручивание: x, y`,
+    window.pageXOffset,
+    window.pageYOffset
+  );
+  /*  console.log(
+    'Ширина и высота viewport',
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+  /* window.scrollTo(
+    section1Coords.left + window.pageXOffset,
+    section1Coords.top + window.pageYOffset
+  ); */
+  /*
+  window.scrollTo({
+    left: section1Coords.left + window.pageXOffset,
+    top: section1Coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  }); */
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/////////////////////////////////////////////////////////
+// Smooth page navigation
+/* document.querySelectorAll('.nav__link').forEach(function (htmlElement) {
+  htmlElement.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  });
+}); */
+
+// Делегирование событий
+// 1. Добавляем event listener для общего родителя
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // 2. Определить target элемент
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href');
+    document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 /*  
 
@@ -128,33 +185,77 @@ logo.classList.contains('a');
 //logo.className = 'a';
 */
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// Виды Событий И Обработчиков Событий
 
-btnScrollTo.addEventListener('click', function (e) {
-  const section1Coords = section1.getBoundingClientRect();
-  console.log(section1Coords);
-  console.log(e.target.getBoundingClientRect());
-  console.log(
-    `Текущее прокручивание: x, y`,
-    window.pageXOffset,
-    window.pageYOffset
-  );
-  /*  console.log(
-    'Ширина и высота viewport',
-    document.documentElement.clientWidth,
-    document.documentElement.clientHeight
-  );
-  /* window.scrollTo(
-    section1Coords.left + window.pageXOffset,
-    section1Coords.top + window.pageYOffset
-  ); */
-  /*
-  window.scrollTo({
-    left: section1Coords.left + window.pageXOffset,
-    top: section1Coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  }); */
+// Новый метод, с анонимным обработчиком
+/* const h1 = document.querySelector('h1');
+h1.addEventListener('mouseenter', function (e) {
+  alert('addEventListener: You are now at the h1 element');
+}); */
 
-  section1.scrollIntoView({ behavior: 'smooth' });
+// Старый метод
+/* h1.onclick = function (e) {
+  alert('addEventListener: You have clicked the h1 element');
+};
+ */
+
+// Новый метод без анонимного обработчика, будет возможность его удалить
+/* const h1 = document.querySelector('h1');
+const alertMouseEnterH1 = function (e) {
+  alert('addEventListener: You are now at the h1 element');
+  h1.removeEventListener('mouseenter', alertMouseEnterH1);
+}; */
+
+/* const h1 = document.querySelector('h1');
+const alertMouseEnterH1 = function (e) {
+  alert('addEventListener: You are now at the h1 element');
+};
+
+h1.addEventListener('mouseenter', alertMouseEnterH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertMouseEnterH1), 3000); */
+
+//////////////////////////////////////////////////////////
+// Event propagation
+// rgb(123, 56, 78)
+
+/* function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomColor = () =>
+  `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(
+    0,
+    255
+  )}, ${getRandomIntInclusive(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = getRandomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+  // Stop propagation()
+  // e.stopPropagation();
 });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = getRandomColor();
+  console.log('Links', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = getRandomColor();
+    console.log('Nav', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+  } //,true
+);
+
+document.querySelector('body').addEventListener('click', function (e) {
+  this.style.backgroundColor = getRandomColor();
+  console.log('Body', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+}); */
