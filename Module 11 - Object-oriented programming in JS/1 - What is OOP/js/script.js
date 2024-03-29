7fd021a1a5312b61570ca601f8cf74ac522400db
Vyacheslav <vsvmrk@gmail.com>
2024-03-29 14:52:19 +0300

    last changes

--- Module 11 - Object-oriented programming in JS/1 - What is OOP/js/script.js
+++ Module 11 - Object-oriented programming in JS/1 - What is OOP/js/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  //console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.age = new Date().getFullYear() - birthYear;
+};
+
+const jack = new Person('Jack', 1974);
+console.log(jack);
+
+// 1. Создаётся новый пустой объект {}
+// 2. Вызывается функция, this = {}
+// 3. {} связывается с прототипом
+// 4. Функция автоматически возвращает {}
