732cb23fa6a8f170dc5bad98dc5af114513a94bb
Vyacheslav <vsvmrk@gmail.com>
2024-05-07 12:24:12 +0300

    Module 11 - 223 Done

--- Module 11 - Object-oriented programming in JS/1 - What is OOP/js/script.js
+++ Module 11 - Object-oriented programming in JS/1 - What is OOP/js/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
-  //console.log(this);
+  //Instance properties (свойства экземпляра)
   this.firstName = firstName;
   this.birthYear = birthYear;
   this.age = new Date().getFullYear() - birthYear;
+
+  // Никогда не делать этого, т.к. с каждым созданным экземпляром будет создаваться
+  // и этот метод, столько, сколько и экземпляров
+  /* this.printAge = function() {
+    console.log(new Date().getFullYear() - this.birthYear);
+  } */
 };
 
 const jack = new Person('Jack', 1974);
@@ -14,3 +20,30 @@ console.log(jack);
 // 2. Вызывается функция, this = {}
 // 3. {} связывается с прототипом
 // 4. Функция автоматически возвращает {}
+
+const mary = new Person('Mary', 2000);
+const john = new Person('John', 2003);
+const vasya = 'Vasya';
+console.log(mary, john);
+
+console.log(mary instanceof Person);
+console.log(vasya instanceof Person);
+/* jack.printAge(); */
+
+// Прототипы
+console.log(Person.prototype);
+
+Person.prototype.printAge = function () {
+  console.log(new Date().getFullYear() - this.birthYear);
+};
+
+jack.printAge();
+console.log(jack.__proto__);
+console.log(jack.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jack));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.creatureClass = 'Mammal';
+console.log(jack, john);
+console.log(jack.hasOwnProperty('birthYear'));
+console.log(jack.hasOwnProperty('creatureClass'));
